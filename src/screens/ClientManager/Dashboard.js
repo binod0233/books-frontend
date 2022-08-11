c9561dcce5cee132595496862deaae6480aa8e2c
Anup <anup05@copenned.com>
2022-08-11 13:05:47 +0545

    worked in dashboard css and button

--- src/screens/ClientManager/Dashboard.js
+++ src/screens/ClientManager/Dashboard.js
@@ -7,6 +7,7 @@ import {Row,Col, DropdownButton, Dropdown} from 'react-bootstrap';
 import Sidebar from '../../components/Sidebar'
 import axios from "axios"
 import { useNavigate, useParams } from 'react-router-dom'
+import { Button } from 'bootstrap'
 
 
 
@@ -116,145 +117,228 @@ const Dashboard = () => {
         </DropdownButton>
         </div>
 
+        <div
+        className="mt-3"
+        style={{
+          fontSize: "22px",
+          fontWeight: "700",
+          background: "white",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <div style={{ width: "20%" }}>Lead Name</div>
+        <div
+          className=""
+          style={{
+            width: "80%",
+            fontSize: "20px",
+            fontWeight: "700",
+            height: "70px",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            textAlign: "center",
+            
+          }}
+        >
+          <div style={{ width: "14%", paddingRight:'.5rem' }}>Date</div>
+          <div style={{ width: "14%",paddingRight:'.5rem' }}>Source</div>
+          <div style={{ width: "14%",paddingLeft  :'.5rem' }}>Team Name</div>
+          <div style={{ width: "14%",paddingLeft:'2rem' }}>Status</div>
+          <div style={{ width: "14%" }}>Deal Amount</div>
+          <div style={{ width: "14%" }}>Total Amount</div>
+          <div style={{ width: "14%" }}>Due Amount</div>
+        </div>
+      </div>
+
+
+{leadList?.map((l, key) => (
+  <>
+  
+  <div
+  key={l.id}
+  style={{
+      background: "#fff",
+      height: "70px",
+      color: "#64676B",
+      fontFamily: "Arial",
+    
+    }}
+    className="d-flex mt-2 mb-2 align-items-center"
+  >
+    <div style={{ fontWeight: "", fontSize: "18px", width: "20%" }}>
+      <div className="d-flex justify-content-start align-items-center ">
+        <div className="  ">
+          <div
+            style={{
+              borderRadius: "50%",
+              background: "#ED6F47",
+              height: "45px",
+              width: "45px",
+              color: "#fff",
+              fontWeight: "400",
+            }}
+            className="d-flex justify-content-center align-items-center  "
+          >
+            {l.name.split(" ").map((n) => n[0])}
+          </div>
+        </div>
+        <div
+          style={{
+            color: "black",
+            fontWeight: "700",
+            fontSize: "22px",
+            lineHeight: "25px",
+            marginLeft: "8px",
+          }}
+        >
+          {l.name}
+          <div
+            style={{
+              color: "#666",
+              fontWeight: "400",
+              fontSize: "14px",
+            }}
+          >
+            {l.email}
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div
+      className="d-flex justify-content-start align-items-center"
+      style={{
+        width: "80%",
+        textAlign: "center",
+      }}
+    >
+      <div style={{ fontWeight: "", fontSize: "18px", width: "14%" }}>
+        {new Date(l.createdDate).toLocaleDateString()}
+      </div>
+
+      <div
+        className="d-flex align-items-center justify-content-center"
+        style={{ fontWeight: "", fontSize: "18px", width: "14%" }}
+      >
+        <div>{l.source}</div>
+      </div>
+
+      <div style={{ fontWeight: "", fontSize: "19px", width: "18%" }}>
+       
+      {l.teamLead}
+      </div>
+
+      <div
+        style={{
+          fontWeight: "700",
+          fontSize: "19px",
+          width: "14%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+        className=""
+      >
+
+      {l.paymentStatus}
+      </div>
+
+      <div style={{ fontWeight: "700", fontSize: "18px", width: "14%" }}>
+      ${l.dealValue}
+      </div>
+
+      <div
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          fontSize: "18px",
+          width: "14%",
+          display: "flex",
+          fontWeight: "700"
+        }}
+      >
+        <div>
+        ${l.dealValue - paymentList.filter(p=>p.payee=== l.name).amount}
+        </div>
+        </div>
         
-<Row style={{background:"#fff",height:"70px"}} className="d-flex align-items-center justify-content-center mb-3 mx-2">
-  <Col md={3} style={{fontWeight:"700",fontSize:"20px" }}>
-    Lead Name
-  </Col>
+        <div
+        style={{
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "18px",
+        width: "14%",
+        fontWeight: "700",
+        display: "flex"}} 
+        >
+        ${paymentList.filter(p=>p.payee=== l.name).amount}
+        </div>
+    </div>
+    </div>
+    <hr style={{height:'.2em'}}/>
+    </>
+    
+    ))}
+    
+    <div className='d-flex justify-content-end align-items-center'>
+<button style={{ border:'none', background:'#3144E7', fontSize:'20px', color:'white', borderRadius:'2px', padding:'6px 20px',marginTop:'1rem', marginBottom:'3rem'}}>View All</button>
+</div>
 
-  <Col md={1} style={{fontWeight:"700",fontSize:"20px" }}>
-    Date
-  </Col>
 
-  <Col md={1} style={{fontWeight:"700",fontSize:"20px" }}>
-  Source
-  </Col>
 
-  <Col md={1} style={{fontWeight:"700",fontSize:"20px" }}>
-    Team Name
-  </Col>
+{/*
 
-  <Col md={1} style={{fontWeight:"700",fontSize:"20px" }}>
-   Status
-  </Col>
+{leadList?.map((l)=> 
 
-  <Col md={1} style={{fontWeight:"700",fontSize:"20px" }}>
-   Deal Amount
-  </Col>
-  <Col md={2} style={{fontWeight:"700",fontSize:"20px" }}>
-   Total Amount
-  </Col>
-  <Col md={2} style={{fontWeight:"700",fontSize:"20px" }}>
-   Due Amount
-  </Col>
-
-</Row>
-
-       {/* {leadsdataList.map((l,index)=>
-
-<Row key={l.index} style={{background:"#fff",height:"70px"}} className="d-flex align-items-center justify-content-center mb-3 mx-2">
-  <Col md={3} style={{fontWeight:"",fontSize:"" }}>
-  <Row> 
-       
-       <Col md={3} className="d-flex justify-content-center align-items-center  ">
-       <div style={{borderRadius:"50%",background:"#ED6F47",height:"45px",width:"45px",color:"#fff",fontWeight:"400"}} className="d-flex justify-content-center align-items-center ">
-          {l.shortName}
-          </div>
-          </Col>
-          <Col md={9} style={{color:"",fontWeight:"700"}}>
-          {l.name}
-          <div style={{color:"#666",fontWeight:"400"}}>
-           {l.email}
-          </div>
-          </Col>
-          </Row>
-  </Col>
-
-  <Col md={1} style={{fontWeight:"",fontSize:"19px" }}>
-    {l.serviceType}
-  </Col>
-
-  <Col md={1} style={{fontWeight:"",fontSize:"19px" }} >
-   <img src={l.contactImage} alt="country" className='img-fluid'/>{l.contactNo}
-  </Col>
-
-  <Col md={1} style={{fontWeight:"",fontSize:"19px" }}>
-    Active
-  </Col>
-  <Col md={1} style={{fontWeight:"",fontSize:"19px" }}>
-    Active
-  </Col>
-  <Col md={1} style={{fontWeight:"",fontSize:"19px" }}>
-    Active
-  </Col>
-
-  <Col md={2} style={{fontWeight:"700",fontSize:"19px" }} className="ps-4">
-   {l.dealValue}
-  </Col>
-
-  <Col md={2} style={{fontWeight:"",fontSize:"19px" }}>
-   <Row>
-    <Col md={7}>
+  <Row key={l.id} style={{background:"#fff",height:"70px"}} className="d-flex align-items-center justify-content-center mb-3 mx-2">
+    <Col md={3} style={{fontWeight:"",fontSize:"" }}>
+    <Row> 
+         
+         <Col md={3} className="d-flex justify-content-center align-items-center  ">
+         <div style={{borderRadius:"50%",background:"#ED6F47",height:"45px",width:"45px",color:"#fff",fontWeight:"400"}} className="d-flex justify-content-center align-items-center ">
+            {l.shortName}
+            </div>
+            </Col>
+            <Col md={9} style={{color:"",fontWeight:"700"}}>
+            {l.name}
+            <div style={{color:"#666",fontWeight:"400"}}>
+             {l.email}
+            </div>
+            </Col>
+            </Row>
+    </Col>
   
-  {l.lastFollowup}  
-  </Col>
-  <Col md={5}>
- 
-        </Col>
-        </Row>
-  </Col>
-</Row>
-
-)} */}
-
-{leadList?.map((l,index)=> 
-
-<Row key={l.id} style={{background:"#fff",height:"70px"}} className="d-flex align-items-center justify-content-center mb-3 mx-2">
-  <Col md={3} style={{fontWeight:"",fontSize:"" }}>
-  <Row> 
-       
-       <Col md={3} className="d-flex justify-content-center align-items-center  ">
-       <div style={{borderRadius:"50%",background:"#ED6F47",height:"45px",width:"45px",color:"#fff",fontWeight:"400"}} className="d-flex justify-content-center align-items-center ">
-          {l.shortName}
-          </div>
-          </Col>
-          <Col md={9} style={{color:"",fontWeight:"700"}}>
-          {l.name}
-          <div style={{color:"#666",fontWeight:"400"}}>
-           {l.email}
-          </div>
-          </Col>
-          </Row>
-  </Col>
-
-  <Col md={1} style={{fontWeight:"",fontSize:"19px" }}>
-    {new Date(l.createdDate).toLocaleDateString()}
-  </Col>
-  <Col md={1} style={{fontWeight:"",fontSize:"19px" }} >
-   {l.source}
-  </Col>
-
-  <Col md={1} style={{fontWeight:"",fontSize:"19px" }} >
-   {l.teamLead}
-  </Col>
-
+    <Col md={1} style={{fontWeight:"",fontSize:"19px" }}>
+      {new Date(l.createdDate).toLocaleDateString()}
+    </Col>
+    <Col md={1} style={{fontWeight:"",fontSize:"19px" }} >
+     {l.source}
+    </Col>
   
-  <Col md={1} style={{fontWeight:"",fontSize:"19px" }}>
-    {l.potential}
-  </Col>
-  <Col md={1} style={{fontWeight:"",fontSize:"19px" }}>
-    ${l.dealValue}
-  </Col>
-
-  <Col md={2} style={{fontWeight:"700",fontSize:"19px" }} className="ps-4">
-   {paymentList.filter(p=>p.payee=== l.name).amount}
-  </Col>
-
-  <Col md={2} style={{fontWeight:"",fontSize:"19px" }}>
-  {l.dealValue - paymentList.filter(p=>p.payee=== l.name).amount}
-  </Col>
-</Row>
-)}
+    <Col md={1} style={{fontWeight:"",fontSize:"19px" }} >
+     {l.teamLead}
+    </Col>
+  
+    
+    <Col md={1} style={{fontWeight:"",fontSize:"19px" }}>
+      {l.potential}
+    </Col>
+    <Col md={1} style={{fontWeight:"",fontSize:"19px" }}>
+      ${l.dealValue}
+    </Col>
+  
+    <Col md={2} style={{fontWeight:"700",fontSize:"19px" }} className="ps-4">
+     {paymentList.filter(p=>p.payee=== l.name).amount}
+    </Col>
+  
+    <Col md={2} style={{fontWeight:"",fontSize:"19px" }}>
+    {l.dealValue - paymentList.filter(p=>p.payee=== l.name).amount}
+    </Col>
+  </Row>
+  )}
+*/}
 
 </div>
 
