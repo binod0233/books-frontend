be7524d84b1566d82fb8a183c2e1bcf22264e801
anup05 <anup05@copenned.com>
2022-08-30 15:34:29 +0545

    displayed local storage teamlead name and email in profile

--- src/components/TeamLeadDashboard.js/TeamLeadProfile.js
+++ src/components/TeamLeadDashboard.js/TeamLeadProfile.js
@@ -10,6 +10,8 @@ import HistoryEduIcon from '@mui/icons-material/HistoryEdu';
 const   TeamLeadProfile = () => {
   const navigate = useNavigate()
   const { teamLead } = useParams()
+const user= JSON.parse(localStorage.getItem('user'))
+
   return (
     <div className='ms-3 me-4 row mb-3' style={{ height: "25vh" }}>
       <Row>
@@ -19,8 +21,8 @@ const   TeamLeadProfile = () => {
               <img src="/images/dashboard/avatar.png" alt="avatar" style={{ height: "18vh", width: "18vh", borderRadius: "50%" }} />
             </div>
             <div className='d-flex flex-column '>
-              <div style={{ fontWeight: "700", margin: "0", fontSize: '1.2rem' }}>Sharmila Dangal</div>
-              <p style={{ color: "#64676B", fontSize: "1rem", margin: "0 0 2vh 0" }}>sharmila@copenned.com</p>
+              <div style={{ fontWeight: "700", margin: "0", fontSize: '1.2rem' }}>{teamLead}</div>
+              <p style={{ color: "#64676B", fontSize: "1rem", margin: "0 0 2vh 0" }}>{user.email}</p>
               <span className='d-flex align-items-center pb-2'>4.9 <StarsIcon /></span>
               <Button onClick={() => navigate(`/teamlead/edit/${teamLead}`)} style={{ fontSize: "1rem", background: "#DD2A7B", border: 'none', cursor: 'pointer' }}>
                 <HistoryEduIcon style={{ marginRight: '.3rem' }} />Edit Profile
